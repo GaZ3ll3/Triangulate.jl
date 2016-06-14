03b7a37138366cbf8af06c768755c1dc3b2f94b2
Yimin <zhongym8903@gmail.com>
2016-06-14 03:02:46 -0500

    edit test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,9 +5,8 @@ numberofpoints = 5
 pointlist = [0., 0., 1., 0., 1., 1., 0.5, 1.5, 0., 1.]
 tic()
 mesh = Triangulate.triangulate(0.01, numberofpoints, pointlist)
+mesh = Triangulate.triangulate(0.001, numberofpoints, pointlist)
+mesh = Triangulate.triangulate(0.0001, numberofpoints, pointlist)
 t = toq()
 println(t)
-tic()
-mesh2 = Triangulate.triangulate(0.01, numberofpoints, pointlist)
-t = toq()
-println(t)
+
